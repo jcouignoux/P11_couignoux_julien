ad3e18b25b93ea13fc4dbb695171752974529204
Julien Couignoux <j.couignoux@gmail.com>
2022-07-18 15:23:58 +0200

    add test index url

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,50 @@
 import pytest
-
-from server import app
+from flask import Response
+import server
 
 
 @pytest.fixture
-def client():
+def app():
+    app = server.app
     app.config.update({'TESTING': True})
 
-    with app.test_client() as client:
-        yield client
+    yield app
+
+
+class myReponses(Response):
+
+    @property
+    def clubs(self):
+        clubs = [
+            {
+                "name": "Simply Lift",
+                "email": "john@simplylift.co",
+                "points": "13"
+            },
+            {
+                "name": "Iron Temple",
+                "email": "admin@irontemple.com",
+                "points": "4"
+            },
+            {"name": "She Lifts",
+             "email": "kate@shelifts.co.uk",
+             "points": "12"
+             }
+        ]
+        return clubs
+
+    @property
+    def competitions(self):
+        competitions = [
+            {
+                "name": "Spring Festival",
+                "date": "2020-03-27 10:00:00",
+                "numberOfPlaces": "25"
+            },
+            {
+                "name": "Fall Classic",
+                "date": "2020-10-22 13:30:00",
+                "numberOfPlaces": "13"
+            }
+        ]
+        return competitions
--- tests/unitaires/test_url.py
+++ tests/unitaires/test_url.py
@@ -1,6 +1,5 @@
-import pytest
-from tests.conftest import client
-from server import index
+import server
+from tests.conftest import myReponses
 
 
 def test_index(client):
@@ -8,21 +7,71 @@ def test_index(client):
     assert response.status_code == 200
 
 
-def test_showSummary(client):
-    response = client.get('/showSummary')
-    assert response.status_code == 200
+# def test_showSummary(client, mocker):
+#     clubs = [{
+#         "name": "Simply Lift",
+#         "email": "john@simplylift.co",
+#         "points": "13"
+#     }]
+#     competitions = [
+#         {
+#             "name": "Spring Festival",
+#             "date": "2020-03-27 10:00:00",
+#             "numberOfPlaces": "25"
+#         },
+#         {
+#             "name": "Fall Classic",
+#             "date": "2020-10-22 13:30:00",
+#             "numberOfPlaces": "13"
+#         }
+#     ]
+#     mocker.patch.object(server, 'clubs', clubs)
+#     mocker.patch.object(server, 'competitions', competitions)
+#     response = client.post('/showSummary')
+#     print(response)
+#     assert response.status_code == 400
 
 
-def test_book(client):
-    response = client.get('/book/<competition>/<club>')
-    assert response.status_code == 200
+# def test_book(client):
+#     club = {
+#         "name": "Simply Lift",
+#         "email": "john@simplylift.co",
+#         "points": "13"
+#     }
+#     competition = {
+#         "name": "Spring Festival",
+#         "date": "2020-03-27 10:00:00",
+#         "numberOfPlaces": "25"
+#     }
+#     response = client.get('/book/' + competition['name'] + '/' + club['name'])
+#     assert response.status_code == 200
 
 
-def test_purchasePlaces(client):
-    response = client.get('/purchasePlaces')
-    assert response.status_code == 200
+# def test_purchasePlaces(client, mocker):
+#     clubs = [{
+#         "name": "Simply Lift",
+#         "email": "john@simplylift.co",
+#         "points": "13"
+#     }]
+#     competitions = [
+#         {
+#             "name": "Spring Festival",
+#             "date": "2020-03-27 10:00:00",
+#             "numberOfPlaces": "25"
+#         },
+#         {
+#             "name": "Fall Classic",
+#             "date": "2020-10-22 13:30:00",
+#             "numberOfPlaces": "13"
+#         }
+#     ]
+#     mocker.patch.object(server, 'clubs', clubs)
+#     mocker.patch.object(server, 'competitions', competitions)
+#     response = client.post('/purchasePlaces')
+#     print(response)
+#     assert response.status_code == 400
 
 
-def test_logout(client):
-    response = client.get('/logout')
-    assert response.status_code == 200
+# def test_logout(client):
+#     response = client.get('/logout')
+#     assert response.status_code == 302

5bc86e35201b3915ccfa51da981303ababd62adb
Julien Couignoux <j.couignoux@gmail.com>
2022-07-18 17:00:05 +0200

    add login test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,47 +4,45 @@ import server
 
 
 @pytest.fixture
-def app():
+def client():
     app = server.app
     app.config.update({'TESTING': True})
+    with app.test_client() as client:
+        yield client
 
-    yield app
 
+mocker_clubs = [
+    {
+        'name': 'Simply Lift',
+        'email': 'john@simplylift.co',
+        'points': '13'
+    },
+    {
+        'name': 'Iron Temple',
+        'email': 'admin@irontemple.com',
+        'points': '4'
+    },
+    {
+        'name': 'She Lifts',
+        'email': 'kate@shelifts.co.uk',
+        'points': '12'
+    }
+]
 
-class myReponses(Response):
-
-    @property
-    def clubs(self):
-        clubs = [
-            {
-                "name": "Simply Lift",
-                "email": "john@simplylift.co",
-                "points": "13"
-            },
-            {
-                "name": "Iron Temple",
-                "email": "admin@irontemple.com",
-                "points": "4"
-            },
-            {"name": "She Lifts",
-             "email": "kate@shelifts.co.uk",
-             "points": "12"
-             }
-        ]
-        return clubs
-
-    @property
-    def competitions(self):
-        competitions = [
-            {
-                "name": "Spring Festival",
-                "date": "2020-03-27 10:00:00",
-                "numberOfPlaces": "25"
-            },
-            {
-                "name": "Fall Classic",
-                "date": "2020-10-22 13:30:00",
-                "numberOfPlaces": "13"
-            }
-        ]
-        return competitions
+mocker_competitions = [
+    {
+        "name": "Spring Festival",
+        "date": "2020-03-27 10:00:00",
+        "numberOfPlaces": "25"
+    },
+    {
+        "name": "Fall Classic",
+        "date": "2020-10-22 13:30:00",
+        "numberOfPlaces": "13"
+    },
+    {
+        "name": "Super Smash Force",
+        "date": "3000-10-22 13:30:00",
+        "numberOfPlaces": "18"
+    }
+]
---
+++ tests/unitaires/test_login.py
@@ -0,0 +1,14 @@
+
+from http import HTTPStatus
+import server
+from tests.conftest import client, mocker_clubs
+
+
+def test_invalid_email(client, mocker):
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    response = client.post(
+        '/showSummary', data={'email': 'unknown@email.com'})
+    data = response.data.decode()
+    expected_template_name = "index.html"
+    assert response.status_code == HTTPStatus.OK
+    assert data.find('Email unknown, please retry.')

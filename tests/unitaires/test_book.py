ae0d233b43cc73f2442b65eba2243258e7097449
Julien Couignoux <j.couignoux@gmail.com>
2022-07-21 18:44:35 +0200

    modify test order

--- tests/unitaires/test_book.py
+++ tests/unitaires/test_book.py
@@ -3,6 +3,20 @@ import server
 from tests.conftest import client, mocker_clubs, mocker_competitions
 
 
+def test_not_exceed_max_places(client, mocker):
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
+    data = {
+        'club': mocker_clubs[0]['name'],
+        'competition': mocker_competitions[0]['name'],
+        'places': 13
+    }
+    response = client.post(
+        '/purchasePlaces', data=data)
+    assert response.status_code == HTTPStatus.OK
+    assert b"You cannot book more than 12 places." in response.data
+
+
 def test_valid_max_point_allowed(client, mocker):
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
@@ -29,17 +43,3 @@ def test_not_enough_points(client, mocker):
         '/purchasePlaces', data=data)
     assert response.status_code == HTTPStatus.OK
     assert b"You doesnt have enough points." in response.data
-
-
-def test_not_exceed_max_places(client, mocker):
-    mocker.patch.object(server, 'clubs', mocker_clubs)
-    mocker.patch.object(server, 'competitions', mocker_competitions)
-    data = {
-        'club': mocker_clubs[0]['name'],
-        'competition': mocker_competitions[0]['name'],
-        'places': 13
-    }
-    response = client.post(
-        '/purchasePlaces', data=data)
-    assert response.status_code == HTTPStatus.OK
-    assert b"You cannot book more than 12 places." in response.data

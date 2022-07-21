39f5611676a6ef1fce56240b0c7a0516bca744fa
Julien Couignoux <j.couignoux@gmail.com>
2022-07-21 16:39:00 +0200

    clean comments

--- tests/unitaires/test_book.py
+++ tests/unitaires/test_book.py
@@ -3,13 +3,6 @@ import server
 from tests.conftest import client, mocker_clubs, mocker_competitions
 
 
-# class TestBook:
-
-#     @classmethod
-# def setup_class(cls, mocker):
-#     mocker.patch.object(server, 'clubs', mocker_clubs)
-#     mocker.patch.object(server, 'competitions', mocker_competitions)
-
 def test_valid_max_point_allowed(client, mocker):
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
@@ -22,22 +15,6 @@ def test_valid_max_point_allowed(client, mocker):
         '/purchasePlaces', data=data)
     assert response.status_code == HTTPStatus.OK
     assert b'Great-booking complete!' in response.data
-
-
-# def test_not_enough_places(client, mocker):
-#     mocker.patch.object(server, 'clubs', mocker_clubs)
-#     mocker.patch.object(server, 'competitions', mocker_competitions)
-#     data = {
-#         'club': mocker_clubs[0]['name'],
-#         'competition': mocker_competitions[0]['name'],
-#         'places': 30
-#     }
-#     response = client.post(
-#         '/purchasePlaces', data=data)
-#     assert response.status_code == HTTPStatus.OK
-#     assert response.url == '/purchasePlaces'
-#     assert b'Great-booking complete!' in response.data
-    # assert f"Max places number must be less than {mocker_competitions[0]['numberOfPlaces']}" in response.data.decode()
 
 
 def test_not_enough_points(client, mocker):

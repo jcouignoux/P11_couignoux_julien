814bdd465ed28272a006ccec6c5bb349c4afbbec
Julien Couignoux <j.couignoux@gmail.com>
2022-07-21 18:04:47 +0200

    add updated points rel=flected

---
+++ tests/unitaires/test_club.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+import server
+from tests.conftest import mocker_clubs, mocker_competitions
+
+
+def test_passed_competitions(client, mocker):
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
+    data = {
+        'club': mocker_clubs[0]['name'],
+        'competition': mocker_competitions[0]['name'],
+        'places': 5
+    }
+    response = client.post('/purchasePlaces', data=data)
+    expected_value = int(mocker_clubs[0]['points']) - data['places']
+    assert response.status_code == HTTPStatus.OK
+    assert f'Points available: ' + \
+        str(expected_value) in response.data.decode()

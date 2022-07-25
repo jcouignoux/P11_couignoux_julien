81a462e5dfa42df7f1725931fc363345c9710531
Julien Couignoux <j.couignoux@gmail.com>
2022-07-25 14:50:48 +0200

    add board test

---
+++ tests/unitaires/test_board.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+import server
+from tests.conftest import client, mocker_clubs, mocker_competitions
+
+
+def test_board(client, mocker):
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    data = {
+        'cur_club': mocker_clubs[0]['email']
+    }
+    response = client.post('/board', data=data)
+    assert response.status_code == HTTPStatus.OK
+    assert f'Points Board' in response.data.decode()

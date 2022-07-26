d9baed04b54fc9e5bd5e2d6c10e5a64e05ce141f
Julien Couignoux <j.couignoux@gmail.com>
2022-07-26 12:32:01 +0200

    add locust tasks

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,11 +1,31 @@
 from locust import HttpUser, task
+from tests.conftest import mocker_clubs, mocker_competitions
 
 
 class ProjectPerfTest(HttpUser):
+    data = {
+        'club': mocker_clubs[0]['name'],
+        'competition': mocker_competitions[0]['name'],
+        'places': 5
+    }
+
     @task
     def home(self):
-        self.client.get("/")
+        self.client.get('/')
 
-    @task(3)
-    def login(self):
-        response = self.client.get("/")
+    @task
+    def showSummary(self):
+        response = self.client.post('/showSummary', self.data)
+
+    @task
+    def book(self):
+        response = self.client.get(
+            '/book/' + self.data['competition'] + '/' + self.data['club'])
+
+    @task
+    def purchasePlaces(self):
+        response = self.client.post('/purchasePlaces', self.data)
+
+    @task
+    def purchasePlaces(self):
+        response = self.client.post('/logout')

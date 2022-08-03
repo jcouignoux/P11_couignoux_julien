2a2a99837c4523caa6d907f5a07a5f4f431adbe5
Julien Couignoux <j.couignoux@gmail.com>
2022-08-03 15:03:05 +0200

    update mocker to upload and fix error

--- tests/integration/test_booking.py
+++ tests/integration/test_booking.py
@@ -1,38 +1,38 @@
 from http import HTTPStatus
-import server
-from tests.conftest import mocker_clubs, mocker_competitions
+from server import loadClubs, loadCompetitions
 
 
-def test_booking(client, mocker):
-    mocker.patch.object(server, 'clubs', mocker_clubs)
-    mocker.patch.object(server, 'competitions', mocker_competitions)
+def test_booking(client):
     data = {
-        'club': mocker_clubs[0]['name'],
-        'competition': mocker_competitions[0]['name'],
+        'club': loadClubs()[0],
+        'competition': loadCompetitions()[0],
         'places': 5
     }
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
 
     response = client.post(
-        '/showSummary', data=mocker_clubs[0])
+        '/showSummary', data=data['club'])
     assert response.status_code == HTTPStatus.OK
     assert f'Welcome,' in response.data.decode()
 
     response = client.get(
-        '/book/' + data['competition'] + '/' + data['club'])
+        '/book/' + data['competition']['name'] + '/' + data['club']['name'])
     assert response.status_code == HTTPStatus.OK
     assert f'How many places?' in response.data.decode()
 
+    print(data)
     response = client.post(
-        '/purchasePlaces', data=data)
+        '/purchasePlaces', data={
+            'club': data['club']['name'],
+            'competition': data['competition']['name'],
+            'places': data['places']}
+    )
     assert response.status_code == HTTPStatus.OK
     assert b'Great-booking complete!' in response.data
-
-    response = client.post('/purchasePlaces', data=data)
-    assert response.status_code == HTTPStatus.OK
     assert f'Points available: ' + \
-        str(mocker_clubs[0]['points']) in response.data.decode()
+        str(int(data['club']['points']) -
+            data['places']*3) in response.data.decode()
 
     response = client.get('/logout')
     assert response.status_code == 302
